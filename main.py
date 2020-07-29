c10ea188d196cffb141f3986efb1de862875eb20
Alejandro Baltra <abaltra@billtrust.com>
2020-07-29 00:50:18 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import requests
+from io import BytesIO
+from PIL import Image, ImageFont, ImageDraw
+import textwrap
+import random
+import tweepy
+
+def get_image():
+    print("Getting image")
+    response = requests.get('https://picsum.photos/800/600.jpg?blur=2', stream=True)
+    return response.content
+
+def get_inspo():
+    print("Getting quote")
+    response = requests.get('https://api.quotable.io/random')
+    return response.json()
+
+def be_aesthetic():
+    image = get_image()
+    inspo = get_inspo()
+
+    print("Being aesthethic")
+
+    # well need to wrap long texts
+    wrapped_inspo = textwrap.wrap(inspo['content'], 50)
+    author = f"- {inspo['author']}"
+
+    img = Image.open(BytesIO(image))
+    font = ImageFont.truetype('Caveat-Regular.ttf', 40)
+
+    color = 'rgb(255, 255, 255)'
+    draw = ImageDraw.Draw(img, 'RGBA')
+
+    # make sure it fits
+    base_y = random.randint(50, 550 - 45 * len(wrapped_inspo) - 45)
+    draw.rectangle([(0, 0), (800, 600)], (0, 0, 0, 125))
+    draw.text((50, base_y), "\n".join(wrapped_inspo), fill=color, font=font)
+    draw.text((350, base_y + 45 * len(wrapped_inspo)), author, fill=color, font=font)
+
+    img.save('aesthetic.jpg')
+
+def share_inspo():
+    twitter_auth_keys = { 
+        "consumer_key"        : "uthink",
+        "consumer_secret"     : "ill",
+        "access_token"        : "upload",
+        "access_token_secret" : "these?"
+    }
+ 
+    auth = tweepy.OAuthHandler(
+            twitter_auth_keys['consumer_key'],
+            twitter_auth_keys['consumer_secret']
+            )
+    auth.set_access_token(
+            twitter_auth_keys['access_token'],
+            twitter_auth_keys['access_token_secret']
+            )
+    api = tweepy.API(auth)
+
+    print("Tweeting")
+    media = api.media_upload("aesthetic.jpg")
+    api.update_status(media_ids=[media.media_id])
+
+def main():
+    be_aesthetic()
+    share_inspo()
+
+main()  
---
+++ readme.md
@@ -0,0 +1,20 @@
+# So Aesthetic
+
+Simple script that will get a random image, slap a random quote on top and share it on twitter
+
+## How to use
+
+Just install the dependencies, insert your twitter dev creds and starts inspiring people with your super deep and aesthetic images
+
+```shell
+pip install -r requirements.txt
+python main.py
+```
+
+## Why tho
+
+Was bored, and wanted to play with pillow's image manipulation tools. Also APIs.
+
+## License
+
+Copy and re-copy, feel free to do whatever you want with this code.
